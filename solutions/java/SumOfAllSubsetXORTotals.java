6eb30d124be639583eca1fc378e1bbb42fe4666b
Roei Shchory <roei.shchory@gmail.com>
2024-05-20 22:57:29 +0300

    daily question 20-MAY-2024

---
+++ solutions/java/SumOfAllSubsetXORTotals.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        generateSubsets(nums, 0, new ArrayList<>(), subsets);
+        int res = 0;
+        int temp;
+        for (List<Integer> subset : subsets){
+            temp = 0;
+            for (int num : subset)
+                temp ^= num;
+            res += temp;
+        }
+        return res;
+    }
+
+    private void generateSubsets(int[] nums, int index, List<Integer> subset, List<List<Integer>> subsets){
+        if (index == nums.length){
+            subsets.add(new ArrayList<>(subset));
+            return;
+        }
+        subset.add(nums[index]);
+        generateSubsets(nums, index + 1, subset, subsets);
+        subset.remove(subset.size() - 1);
+        generateSubsets(nums, index + 1, subset, subsets);
+    }
+
+}
