a4f323f324d928f29a2c80a3470034173689ba3f
Roei Shchory <roei.shchory@gmail.com>
2024-06-02 10:14:29 +0300

    daily question 02-JUN-2024 (previously solved)

--- solutions/c/ReverseString.c
+++ solutions/c/ReverseString.c
@@ -1,7 +1,7 @@
 void reverseString(char* s, int sSize) {
     char temp = '\0';
     // for (int i = 0; i < sSize / 2; i++){
-    for (int i = sSize / 2 - 1; i >= 0; i--){
+    for (int i = sSize / 2 - 1; 0 <= i; i--){
         temp = s[sSize - 1 - i];
         s[sSize - 1 - i] = s[i];
         s[i] = temp;
