9c28b592a16bd7c669bdb4f061e4b11e91c97f48
Roei Shchory <roei.shchory@gmail.com>
2024-07-13 14:44:29 +0300

    removed print, fixed comment

--- solutions/python3/RobotCollisions.py
+++ solutions/python3/RobotCollisions.py
@@ -7,14 +7,12 @@ class Solution:
         pdh = sorted([list(e) for e in zip(positions, directions, healths, range(len(healths)))], key=lambda x: x[0])
         i = 0
 
-        print(pdh)
-
         while i < len(pdh) - 1:
             if pdh[i][1] == "R" and pdh[i + 1][1] == "L":  # collision
                 if pdh[i][2] > pdh[i + 1][2]:   # "R" won (current index)
                     pdh[i][2] -= 1
                     pdh.pop(i + 1)
-                elif pdh[i][2] < pdh[i + 1][2]: # "L" win (next index)
+                elif pdh[i][2] < pdh[i + 1][2]: # "L" won (next index)
                     pdh[i + 1][2] -= 1
                     pdh.pop(i)
                     i = max(0, i - 1)
